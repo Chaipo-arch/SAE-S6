58b916219b6dda509407274181fe58ce8a11827c
enzo2900 <133020135+enzo2900@users.noreply.github.com>
2025-05-15 10:15:58 +0200

    Fix: Correction des tests qui ne fonctionnerait pas si plusieurs PatientHistoryDAO existait => Les mêmes données seraient créées plusieurs fois.

--- legacy-software/src/test/java/sae/semestre/six/domain/patient/history/PatientHistoryDaoTest.java
+++ legacy-software/src/test/java/sae/semestre/six/domain/patient/history/PatientHistoryDaoTest.java
@@ -27,7 +27,7 @@ public class PatientHistoryDaoTest {
     @Autowired
     private List<PatientHistoryDao> daoImplementations;
 
-    @PersistenceContext
+     @PersistenceContext
     private EntityManager entityManager;
 
     private Patient testPatient;
@@ -42,25 +42,24 @@ public class PatientHistoryDaoTest {
     }
     @Test
     void testFindCompleteHistoryByPatientId() {
+        PatientHistory history = PatientHistory.builder().patient(testPatient).visitDate(new Date())
+                .diagnosis("Covid-19").symptoms("fièvre").build();
+        Bill bill = Bill.builder().totalAmount(150.0)
+                .billDate(new Date()).patientHistory(history).build();
+        PatientHistory history2 = PatientHistory.builder().patient(testPatient).visitDate(new Date())
+                .diagnosis("Fièvre jaune").symptoms("fièvre").build();
+        Bill bill2 = Bill.builder().totalAmount(150.0)
+                .billDate(new Date()).patientHistory(history).build();
+
+        history.addBill(bill);
+        history2.addBill(bill2);
+
+        entityManager.persist(history);
+        entityManager.persist(bill);
+        entityManager.persist(history2);
+        entityManager.persist(bill2);
+        entityManager.flush();
         for (PatientHistoryDao dao : daoImplementations) {
-            PatientHistory history = PatientHistory.builder().patient(testPatient).visitDate(new Date())
-                    .diagnosis("Covid-19").symptoms("fièvre").build();
-            Bill bill = Bill.builder().totalAmount(150.0)
-                    .billDate(new Date()).patientHistory(history).build();
-            PatientHistory history2 = PatientHistory.builder().patient(testPatient).visitDate(new Date())
-                    .diagnosis("Fièvre jaune").symptoms("fièvre").build();
-            Bill bill2 = Bill.builder().totalAmount(150.0)
-                    .billDate(new Date()).patientHistory(history).build();
-
-            history.addBill(bill);
-            history2.addBill(bill2);
-
-            entityManager.persist(history);
-            entityManager.persist(bill);
-            entityManager.persist(history2);
-            entityManager.persist(bill2);
-            entityManager.flush();
-
             List<PatientHistory> results = dao.findCompleteHistoryByPatientId(testPatient.getId());
 
             assertEquals(2,results.size(), "" + dao.getClass());
@@ -72,14 +71,13 @@ public class PatientHistoryDaoTest {
 
     @Test
     void testSearchByMultipleCriteria_shouldMatchDiagnosis() {
+        PatientHistory history = PatientHistory.builder().patient(testPatient)
+                .visitDate(new GregorianCalendar(2025, Calendar.MARCH, 1).getTime())
+                .diagnosis("Pneumonie aiguë").symptoms("Toux, douleurs thoraciques").build();
+
+        entityManager.persist(history);
+        entityManager.flush();
         for (PatientHistoryDao dao : daoImplementations) {
-            PatientHistory history = PatientHistory.builder().patient(testPatient)
-                    .visitDate(new GregorianCalendar(2025, Calendar.MARCH, 1).getTime())
-                    .diagnosis("Pneumonie aiguë").symptoms("Toux, douleurs thoraciques").build();
-
-            entityManager.persist(history);
-            entityManager.flush();
-
             List<PatientHistory> results = dao.searchByMultipleCriteria(
                     "pneumo",
                     new GregorianCalendar(2025, Calendar.JANUARY, 1).getTime(),
@@ -108,30 +106,29 @@ public class PatientHistoryDaoTest {
 
     @Test
     void testFindDataForHistoryEntry() {
+        PatientHistory patientHistory = new PatientHistoryInformation(new Date(),
+                "Diagnosis","Symptoms","Notes").toPatientHistory(testPatient);
+        entityManager.persist(patientHistory);
+        Doctor doctor = Doctor.builder().doctorNumber("Doc011").firstName("e").lastName("l").email("email@gmail.com").build();
+        Prescription prescription = Prescription.builder().prescriptionNumber("P011").patient(testPatient).build();
+        Treatment treatment = Treatment.builder().name("TreatmentName").patientHistory(patientHistory).treatmentDate(new Date()).build();
+
+        LabResult labResult = LabResult.builder().testName("Test").patientHistory(patientHistory).build();
+        Appointment appointment = Appointment.builder().appointmentNumber("App011").doctor(doctor)
+                .patient(testPatient).patientHistory(patientHistory).description("DescriptionApp")
+                .status("CONFIRMÉ").appointmentDate(new Date()).build();
+        Bill bill = Bill.builder().billNumber("Bil011")
+                .patientHistory(patientHistory).patient(testPatient)
+                .totalAmount(10.0).status("EN ATTENTE").createdDate(new Date()).build();
+        entityManager.persist(patientHistory);
+        entityManager.persist(doctor);
+        entityManager.persist(appointment);
+        entityManager.persist(treatment);
+        entityManager.persist(prescription);
+        entityManager.persist(bill);
+        entityManager.persist(labResult);
+        entityManager.flush();
         for(PatientHistoryDao dao : daoImplementations) {
-            PatientHistory patientHistory = new PatientHistoryInformation(new Date(),
-                    "Diagnosis","Symptoms","Notes").toPatientHistory(testPatient);
-            dao.save(patientHistory);
-            Doctor doctor = Doctor.builder().doctorNumber("Doc011").firstName("e").lastName("l").email("email@gmail.com").build();
-            Prescription prescription = Prescription.builder().prescriptionNumber("P011").patient(testPatient).build();
-            Treatment treatment = Treatment.builder().name("TreatmentName").patientHistory(patientHistory).treatmentDate(new Date()).build();
-
-            LabResult labResult = LabResult.builder().testName("Test").patientHistory(patientHistory).build();
-            Appointment appointment = Appointment.builder().appointmentNumber("App011").doctor(doctor)
-                    .patient(testPatient).patientHistory(patientHistory).description("DescriptionApp")
-                    .status("CONFIRMÉ").appointmentDate(new Date()).build();
-            Bill bill = Bill.builder().billNumber("Bil011")
-                    .patientHistory(patientHistory).patient(testPatient)
-                    .totalAmount(10.0).status("EN ATTENTE").createdDate(new Date()).build();
-
-            entityManager.persist(patientHistory);
-            entityManager.persist(doctor);
-            entityManager.persist(appointment);
-            entityManager.persist(treatment);
-            entityManager.persist(prescription);
-            entityManager.persist(bill);
-            entityManager.persist(labResult);
-            entityManager.flush();
             HistoryEntry historyEntry = dao.getHistoryEntryData(prescription.getId(),treatment.getId(),labResult.getId(),bill.getId(),appointment.getId());
 
             assertEquals("Bil011",historyEntry.bill().getBillNumber(), "La récupération du bill ne se fait pas correctement " + dao.getClass());
@@ -139,6 +136,8 @@ public class PatientHistoryDaoTest {
             assertEquals("DescriptionApp",historyEntry.appointment().getDescription(), "La récupération du appointment ne se fait pas correctement " + dao.getClass());
             assertEquals("TreatmentName",historyEntry.treatment().getName(), "La récupération du treatment ne se fait pas correctement " + dao.getClass());
             assertEquals("P011",historyEntry.prescription().getPrescriptionNumber(), "La récupération de la prescription ne se fait pas correctement " + dao.getClass());
+
         }
     }
+
 }
