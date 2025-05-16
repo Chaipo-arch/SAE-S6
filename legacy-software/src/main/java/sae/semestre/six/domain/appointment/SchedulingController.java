54eac67f14eec7f6a42a08e1db332e8bb48246be
Chaipo <ahmed.bribach@iut-rodez.fr>
2025-05-16 14:23:41 +0200

    fix : verify room avalability

--- legacy-software/src/main/java/sae/semestre/six/domain/appointment/SchedulingController.java
+++ legacy-software/src/main/java/sae/semestre/six/domain/appointment/SchedulingController.java
@@ -6,6 +6,8 @@ import sae.semestre.six.domain.doctor.DoctorDao;
 import sae.semestre.six.domain.doctor.Doctor;
 import sae.semestre.six.domain.patient.Patient;
 import sae.semestre.six.domain.patient.PatientDao;
+import sae.semestre.six.domain.room.Room;
+import sae.semestre.six.domain.room.RoomDao;
 import sae.semestre.six.mail.EmailService;
 
 import java.time.LocalDate;
@@ -23,26 +25,31 @@ public class SchedulingController {
     private final DoctorDao doctorDao;
     private final PatientDao patientDao;
     private final EmailService emailService;
+    private final RoomDao roomDao;
 
     public SchedulingController(
             AppointmentDao appointmentDao,
             DoctorDao doctorDao, PatientDao patientDao,
-            EmailService emailService) {
+            EmailService emailService,
+            RoomDao roomDao) {
         this.appointmentDao = appointmentDao;
         this.doctorDao = doctorDao;
         this.patientDao = patientDao;
         this.emailService = emailService;
+        this.roomDao = roomDao;
     }
 
     @PostMapping("/appointment")
     public String scheduleAppointment(
             @RequestParam Long doctorId,
             @RequestParam Long patientId,
+            @RequestParam Long roomId,
             @RequestParam
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime appointmentDateTime) {
         // Vérifier la disponibilité du docteur
         Doctor doctor = doctorDao.findById(doctorId);
         Patient patient = patientDao.findById(patientId);
+        Room room = roomDao.findById(roomId);
         List<Appointment> doctorAppointments = appointmentDao.findByDoctorId(doctorId);
         boolean conflict = doctorAppointments.stream()
                 .anyMatch(existing -> existing.getAppointmentDate().toLocalDate().equals(appointmentDateTime.toLocalDate()) &&
@@ -53,6 +60,12 @@ public class SchedulingController {
         }
         if(doctor == null || patient == null) {
             return "Doctor or patient not found";
+        }
+        if(room == null) {
+            return "Room not found";
+        }
+        if(!room.getAvailability().canAcceptPatient()){
+            return "Room is not available";
         }
 
         LocalTime time = appointmentDateTime.toLocalTime();
@@ -66,6 +79,7 @@ public class SchedulingController {
         appointment.setPatient(patient);
         appointment.setAppointmentNumber("APPT" + System.currentTimeMillis());
         appointment.setStatus("SCHEDULED");
+
         doctor.getAppointments().add(appointment);
         appointmentDao.save(appointment);
         // Envoyer un email de confirmation
